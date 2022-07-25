abf81ebf7a80952ed259303fa501b4f6553b4d51
Júlio S Santos <93954454+K7Ju@users.noreply.github.com>
2022-07-25 11:22:11 -0300

    Add files via upload

---
+++ DevC/Struct e sub-struct/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Aluno{
+    int id;
+    int dia;
+    int mes;
+    int ano;
+};
+
+int main(){
+    //Criando um aluno
+    struct Aluno aluno1;
+
+    //Modificando valores
+    aluno1.d1 = 15;
+
+    return 0;
+}
